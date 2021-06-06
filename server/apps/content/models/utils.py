31611b650560c7ab73e882c79208cd0faaab998b
Alexander Shcherbanyuk <s0ul@protonmail.com>
2021-06-06 18:16:11 +0700

    Added model corrects

--- server/apps/content/models/utils.py
+++
@@ -1,6 +0,0 @@
-import uuid
-
-
-def get_upload_file_path(instance, filename: str) -> str:
-    folder = f'{instance.__class__.__name__.lower()}s'
-    return f'{folder}/{uuid.uuid4().hex}_{filename}'
